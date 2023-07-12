018db1a42ec0825fd5a5206cd102c11d20cceff7
AIT EL BACHA <r.aitelbacha@gmail.com>
2023-07-12 15:06:14 +0200

    add data user in object connect

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@
 import RouterAll from './router/Router';
 import { Provider } from 'react-redux';
 import { store } from './store/reducer.tsx';
-import dotenv from 'dotenv';
-dotenv.config();
 
 function App() {
   return (
--- src/action/action.tsx
+++ src/action/action.tsx
@@ -1,4 +1,4 @@
 export const ajouter =()=> { return { type :'ajouter'} }
 export const reduit =()=>  { return { type :'reduit'}  }
-export const connecter =()=>  { return { type :'connecter'}  }
+export const connecter =(user)=>  { return { type :'connecter'  , user:user}  }
 export const deconnecter  =()=>  { return { type :'deconnecter'}  }
