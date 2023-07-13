1d51600ec19bfdde5290e8ade60ccee004ee01b1
AIT EL BACHA <r.aitelbacha@gmail.com>
2023-07-13 14:46:50 +0200

    delete inscrit

--- server/router/user.js
+++ server/router/user.js
@@ -17,21 +17,7 @@ app.post('/users/:email', async (req, res) => {
   }
 });
 
-app.post('/users/inscrit', async (req, res) => {
-  const email = req.params.email
-  try {
-    const userWithAchat = await prisma.user.findUnique({
-      where: { email: email },
-      include: {
-        Achat: true,
-      },
-    });
-    res.status(200).send({ status: 'success', data: userWithAchat });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des utilisateurs.' });
-  }
-});
+
 
 }
 
