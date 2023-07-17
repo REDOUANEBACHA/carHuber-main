5f9dca0f1b9ecd3433758ed2b22785de69739eb0
AIT EL BACHA <r.aitelbacha@gmail.com>
2023-07-17 11:05:56 +0200

    typer data

--- src/action/action.tsx
+++ src/action/action.tsx
@@ -1,4 +1,6 @@
+import { data } from "../type/actionType"
+
 export const ajouter =()=> { return { type :'ajouter'} }
 export const reduit =()=>  { return { type :'reduit'}  }
-export const connecter =(user)=>  { return { type :'connecter'  , user:user}  }
+export const connecter =(data:data)=>  { return { type :'connecter'  , data:data}  }
 export const deconnecter  =()=>  { return { type :'deconnecter'}  }
