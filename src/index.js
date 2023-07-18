5cfa34ecdac88bfac0e31b2bd5ffadb3b99057d3
AIT EL BACHA <r.aitelbacha@gmail.com>
2023-07-18 11:08:09 +0200

    integrer tailwind

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './compenent/Header'
-import Footer from './compenent/Footer'
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Header/>
-  
       <App />
-    <Footer/>
-   
   </React.StrictMode>
 );
 
