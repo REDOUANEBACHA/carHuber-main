4bb3eead9b8de02dedd63a5cdf18ee6b28bd53d8
AIT EL BACHA <r.aitelbacha@gmail.com>
2023-07-28 17:23:28 +0200

    add in state

--- src/store/state.tsx
+++ src/store/state.tsx
@@ -1,9 +1,11 @@
-import { data } from "../type/actionType";
-
+import { produit, data } from "../type/actionType";
 
 export const initialState = {
+  selectCategorie : 'Electronique',
   panier: 0,
   data:null as data | null ,
   connecter: false,
+  produit : [] as produit[] ,
+  categorie : [] as any []
 };
 export type AppState = typeof initialState;
